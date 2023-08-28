d162ce471962e8e74967e24a12ee7e963cee363a
Henry Ripple <hripple46@gmail.com>
2023-08-28 13:01:17 -0500

    fetching questions via backend

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path");
+var cors = require("cors");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
@@ -7,6 +8,8 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,70 @@
+let fetch;
+
+async function init() {
+  const nodeFetch = await import("node-fetch");
+  fetch = nodeFetch.default;
+}
+
 var express = require("express");
 var router = express.Router();
+
+//empty questions array initialization
+var questions = [];
+//function to add questions to the array every 24 hours
+const addQuestions = () => {
+  fetch("https://opentdb.com/api.php?amount=5&category=9")
+    .then((response) => response.json())
+    .then((data) => {
+      // Sort the questions by difficulty level
+      const sortedByDifficulty = data.results.sort((a, b) => {
+        const order = ["easy", "medium", "hard"];
+        return order.indexOf(a.difficulty) - order.indexOf(b.difficulty);
+      });
+
+      // Shuffle answers for each question
+      const shuffledQuestions = sortedByDifficulty.map((question) => {
+        const allAnswers = [
+          // Spread operator to add all incorrect answers
+          ...question.incorrect_answers,
+          question.correct_answer,
+        ];
+        question.shuffledAnswers = shuffle([...allAnswers]);
+        return question;
+      });
+
+      // Set the sorted and shuffled questions into state
+      questions = shuffledQuestions;
+    });
+};
+// Fisher-Yates shuffle algorithm
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+//call function
+init().then(() => {
+  addQuestions();
+});
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.send("3 of 5 Correct");
+});
+
+router.get("/questions", async function (req, res, next) {
+  // Check if the questions array is empty
+  const checkIfEmpty = async () => {
+    if (questions.length === 0) {
+      // If it is, call the addQuestions function
+      addQuestions();
+    }
+  };
+  //call function
+  await checkIfEmpty();
+  res.status(200).json(questions);
 });
 
 module.exports = router;
