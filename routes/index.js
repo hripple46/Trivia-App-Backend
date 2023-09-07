aacbeb326f1e42c55e4ccd4a91b07686f736e817
Henry Ripple <hripple46@gmail.com>
2023-09-07 18:14:54 -0500

    changed time for testing

--- routes/index.js
+++ routes/index.js
@@ -120,7 +120,7 @@ function shuffle(array) {
 init()
   .then(() => {
     cron.schedule(
-      "15 19 * * *",
+      "20 19 * * *",
       async () => {
         console.log("Fetching new questions...");
         await addQuestions();
