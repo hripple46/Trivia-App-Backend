5703812e506eb5f79591b1358c64f4e62b74a194
Henry Ripple <hripple46@gmail.com>
2023-09-07 18:25:55 -0500

    reverted tining to 1am

--- routes/index.js
+++ routes/index.js
@@ -120,7 +120,7 @@ function shuffle(array) {
 init()
   .then(() => {
     cron.schedule(
-      "20 19 * * *",
+      "0 1 * * *",
       async () => {
         console.log("Fetching new questions...");
         await addQuestions();
